e6b3c1fb39b7ebfd714134502e40bbd738334195
bupz782 <non>
2025-11-17 14:49:16 +0100

    feat(008): add orchestrator

--- crates/application/src/lib.rs
+++ crates/application/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod commands;
+pub mod dto;
+pub mod queries;
+pub mod use_cases;
+pub mod orchestrator;
