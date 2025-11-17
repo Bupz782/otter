c4afa6f43b71ed8345a47e91202c8c8c38e6f02b
bupz782 <non>
2025-11-17 16:21:05 +0100

    fix(lint): fmt check

--- crates/application/src/lib.rs
+++ crates/application/src/lib.rs
@@ -1,6 +1,5 @@
 pub mod commands;
 pub mod dto;
+pub mod orchestrator;
 pub mod queries;
 pub mod use_cases;
-pub mod orchestrator;
-
--- crates/domain/src/ports/mod.rs
+++ crates/domain/src/ports/mod.rs
@@ -1,5 +1,5 @@
-pub mod zkp_port;
-pub mod fhe_port;
 pub mod evm_port;
+pub mod fhe_port;
 pub mod intent_parser_port;
 pub mod mempool_port;
+pub mod zkp_port;
