58e9c504c60cff6bd59a9f217dacf7cdb1c155b8
Bupz <98107361+Bupz782@users.noreply.github.com>
2026-01-20 15:15:13 +0100

    Epic 0.2 - Epic 1.1 - Epic 1.2 - Epic 1.3 (#227)
    * feat(005): add labs folders
    
    * feat(006): add folders for archi hexa
    
    * feat(007): add ports
    
    * feat(007): add ports
    
    * feat(007): add ports
    
    * feat(007): add ports
    
    * feat(007): add ports
    
    * feat(008): add orchestrator (#219)
    
    Co-authored-by: bupz782 <non>
    
    * feat(009): add loggers (#221)
    
    Co-authored-by: bupz782 <non>
    
    * feat(010): add learning.md
    
    * feat(012): add clippy to ci
    
    * fix(syntax): redundance declaration
    
    * feat(013): add fmt to ci
    
    * fix(lint): fmt check
    
    * feat(014): add benchmark.sh
    
    * epic1.1 (#224)
    
    * feat(domain): US(015) define Intent enum with Asset and Protocol
    
    * feat(domain): US-016  define Condition struct with Metric and Comparator
    
    * feat(domain): US-017 refactor Protocol with type-safe DexType and LendingType
    
    * feat(domain): US-017 define ExecutionPlan with Step and StepAction
    
    * feat(domain): 019 add IntentValidator service skeleton
    
    * fix(ci): fix the lint
    
    * fix(ci): fix the lint
    
    ---------
    
    Co-authored-by: arthur.bertier <arthur.bertier@emeria.eu>
    
    * Vague-1/epic 1.2 parser (#225)
    
    * feat(infra): implement regex parser for Lend intent US-020
    
    * feat(infra): implement regex parser for Swap intent US-021
    
    * feat(infra): implement regex parser for Borrow intent US-022
    
    * feat(infra): implement regex parser for Stake intent US-023
    
    * feat(infra): implement regex parser for Condition US-024
    
    * fix(infra): add 'if' prefix when parsing conditions in conditional intents US-025
    
    ---------
    
    * Vague 1/epic 1.3 amount error handling (#226)
    
    * feat(domain): migrate to u128 for amount handling to support large token values
    
    * feat(infra): errors handling for the parser us-027
    
    * fix(pipeline) fmt fix
    
    * fix(pipeline): clippy
    
    ---------
    
    Co-authored-by: bupz782 <none>
    Co-authored-by: bupz782 <non>

--- crates/domain/src/models/mod.rs
+++ crates/domain/src/models/mod.rs
@@ -1,1 +1,6 @@
-
+pub mod intent;
+pub use intent::{Asset, ConditionalIntent, DexType, Intent, LendingType};
+pub mod condition;
+pub use condition::{Comparator, Condition, Metric};
+pub mod execution_plan;
+pub use execution_plan::{Address, ExecutionPlan, Step, StepAction};
